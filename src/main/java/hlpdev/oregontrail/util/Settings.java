fb7f83c030e71c10c2159d51a5e7e32ebd0b3a22
= <hunterpollock2006@gmail.com>
2025-02-11 22:32:17 -0500

    Created the settings class for storing fontSize / ui scaling and whatever i may add in the future

---
+++ src/main/java/hlpdev/oregontrail/util/Settings.java
@@ -0,0 +1,44 @@
+package hlpdev.oregontrail.util;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Settings implements Serializable {
+    public static Settings Settings = null;
+
+    public static void Load() throws IOException, ClassNotFoundException {
+        Path directory = Path.of(System.getProperty("user.home"), "/hlpdev/oregontrail/");
+        Path path = Path.of(directory.toString(), "settings.bin");
+
+        if (Files.notExists(path)) {
+            Settings = new Settings();
+            Settings.FontSize = 12;
+
+            Save();
+        }
+
+        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(path.toString()))) {
+            Settings = (Settings)inputStream.readObject();
+        }
+    }
+
+    public static void Save() throws IOException {
+        Path directory = Path.of(System.getProperty("user.home"), "/hlpdev/oregontrail/");
+        Path path = Path.of(directory.toString(), "settings.bin");
+
+        if (Files.notExists(directory)) {
+            Files.createDirectories(directory);
+        }
+
+        if (Files.notExists(path)) {
+            Files.createFile(path);
+        }
+
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(path.toString()))) {
+            outputStream.writeObject(Settings);
+        }
+    }
+
+    public Integer FontSize;
+}
