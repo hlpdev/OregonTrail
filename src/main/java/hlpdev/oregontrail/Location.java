c7cb4478d1fae88bf027bc86f12d7febbe745893
= <hunterpollock2006@gmail.com>
2025-02-11 22:30:50 -0500

    location enum for tracking progress

---
+++ src/main/java/hlpdev/oregontrail/Location.java
@@ -0,0 +1,16 @@
+package hlpdev.oregontrail;
+
+public enum Location {
+    INDEPENDENCE,
+    FORT_HEARNEY,
+    CHIMNEY_ROCK,
+    LARAMIE,
+    INDEPENDENCE_ROCK,
+    SOUTH_PASS,
+    SODA_SPRINGS,
+    FORT_HALL,
+    FORT_BOISE,
+    BLUE_MOUNTAINS,
+    THE_DALLES,
+    OREGON_CITY
+}
