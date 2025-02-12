9cde171215429aedeea4b2309b6edc78e3c90d56
= <hunterpollock2006@gmail.com>
2025-02-11 22:32:03 -0500

    Created the progress class

---
+++ src/main/java/hlpdev/oregontrail/game/Progress.java
@@ -0,0 +1,10 @@
+package hlpdev.oregontrail.game;
+
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.terminal.Terminal;
+
+public class Progress {
+    public static void Execute(Terminal terminal, Screen screen) {
+
+    }
+}
