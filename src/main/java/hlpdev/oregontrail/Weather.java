d83e7973a7664950d4c7d68e162dc96051607079
= <hunterpollock2006@gmail.com>
2025-02-12 19:36:22 -0500

    Added metadata to the weather enum to store multipliers to use in calculations for player health impact, food consumption impact, sickness chance multiplier, and randomness weight

--- src/main/java/hlpdev/oregontrail/Weather.java
+++ src/main/java/hlpdev/oregontrail/Weather.java
@@ -1,12 +1,48 @@
 package hlpdev.oregontrail;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public enum Weather {
-    CLEAR,
-    SUNNY,
-    RAIN,
-    STORMY,
-    SNOW,
-    FOG,
-    HOT,
-    COLD
+    CLEAR("Clear", 1, 1, 1, 0.5, 7),
+    SUNNY("Sunny", 1.1, 1, 0.9, 0.5, 4),
+    RAIN("Rain", 0.9, 1.1, 1, 1.2, 2),
+    STORMY("Stormy", 0.6, 1.1, 1.2, 1.3, 1),
+    SNOW("Snow", 0.5, 1.3, 1.5, 1.5, 1),
+    FOG("Fog", 0.6, 1, 1, 0.5, 1),
+    HOT("Hot", 0.9, 1.1, 1.2, 1, 2),
+    COLD("Cold", 0.9, 1.4, 1.5, 2, 2);
+
+    public final String visualRepresentation;
+
+    public final double speedMultiplier;
+    public final double healthImpactMultiplier;
+    public final double foodConsumptionMultiplier;
+    public final double sicknessMultiplier;
+    public final int randomnessWeight;
+
+    Weather(String visualRepresentation, double speedMultiplier, double healthImpactMultiplier, double foodConsumptionMultiplier, double sicknessMultiplier, int randomnessWeight) {
+        this.visualRepresentation = visualRepresentation;
+        this.speedMultiplier = speedMultiplier;
+        this.healthImpactMultiplier = healthImpactMultiplier;
+        this.foodConsumptionMultiplier = foodConsumptionMultiplier;
+        this.sicknessMultiplier = sicknessMultiplier;
+        this.randomnessWeight = randomnessWeight;
+    }
+
+    public static Weather getRandomWeather() {
+        List<Weather> weathers = new ArrayList<>();
+        for (Weather weather : Weather.values()) {
+            for (int i = 0; i < weather.randomnessWeight; i++) {
+                weathers.add(weather);
+            }
+        }
+
+        Random random = new Random();
+
+        Collections.shuffle(weathers, random);
+        return weathers.get(random.nextInt(weathers.size()));
+    }
 }
