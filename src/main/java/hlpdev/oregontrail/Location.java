5e6c175d3873eccc841ab6474c24a0e8758e3e69
= <hunterpollock2006@gmail.com>
2025-02-12 19:34:11 -0500

    Added metadata to the Location enum to store information to track player position, location naming and relative location naming

--- src/main/java/hlpdev/oregontrail/Location.java
+++ src/main/java/hlpdev/oregontrail/Location.java
@@ -1,16 +1,69 @@
 package hlpdev.oregontrail;
 
 public enum Location {
-    INDEPENDENCE,
-    FORT_HEARNEY,
-    CHIMNEY_ROCK,
-    LARAMIE,
-    INDEPENDENCE_ROCK,
-    SOUTH_PASS,
-    SODA_SPRINGS,
-    FORT_HALL,
-    FORT_BOISE,
-    BLUE_MOUNTAINS,
-    THE_DALLES,
-    OREGON_CITY
+    INDEPENDENCE(0, "Independence, Missouri", "Missouri Grasslands"),
+    FORT_KEARNEY(100, "Fort Kearney, Nebraska", "Nebraska Grasslands"),
+    CHIMNEY_ROCK(200, "Chimney Rock, Nebraska", "Nebraska Grasslands"),
+    LARAMIE(300, "Laramie, Wyoming", "Wyoming Forests"),
+    INDEPENDENCE_ROCK(400, "Independence Rock, Wyoming", "Wyoming Desert"),
+    SOUTH_PASS(500, "South Pass, Wyoming", "Wyoming Forests"),
+    SODA_SPRINGS(600, "Soda Springs, Idaho", "Idaho Desert"),
+    FORT_HALL(700, "Fort Hall, Idaho", "Idaho Mountains"),
+    FORT_BOISE(800, "Fort Boise, Idaho", "Idaho Mountains"),
+    BLUE_MOUNTAINS(900, "Blue Mountains, Oregon", "Oregon Mountains"),
+    THE_DALLES(1000, "The Dalles, Oregon", "Oregon Forests"),
+    OREGON_CITY(1100, "Oregon City, Oregon", "Oregon Forests");
+
+    public final int location;
+    public final String name;
+    public final String relativeLocation;
+
+    Location(int location, String name, String relativeLocation) {
+        this.location = location;
+        this.name = name;
+        this.relativeLocation = relativeLocation;
+    }
+
+    public static Location getLocation(int location) {
+        for (Location loc : values()) {
+            if (loc.location == location) {
+                return loc;
+            }
+        }
+
+        return null;
+    }
+
+    public static Location getClosestLocation(int location) {
+        Location closest = Location.INDEPENDENCE;
+        for (Location loc : values()) {
+            if (loc.location <= location) {
+                closest = loc;
+            } else {
+                break;
+            }
+        }
+
+        return closest;
+    }
+
+    public static boolean isCloseToPointOfInterest(int distanceTraveled) {
+        for (int x = 0; x <= 1100; x += 100) {
+            if (java.lang.Math.abs(distanceTraveled - x) <= 10) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static Location getPointOfInterest(int distanceTraveled) {
+        for (int x = 0; x <= 1100; x += 100) {
+            if (java.lang.Math.abs(distanceTraveled - x) <= 10) {
+                return Location.getLocation(x);
+            }
+        }
+
+        return null;
+    }
 }
