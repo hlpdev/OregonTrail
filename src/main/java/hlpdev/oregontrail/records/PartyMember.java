e6cf77e2b4c453ff23bf7482b5c9ace3e808fe84
= <hunterpollock2006@gmail.com>
2025-02-12 20:27:35 -0500

    added hasBeenBuried field to the party member record

--- src/main/java/hlpdev/oregontrail/records/PartyMember.java
+++ src/main/java/hlpdev/oregontrail/records/PartyMember.java
@@ -2,20 +2,24 @@ package hlpdev.oregontrail.records;
 
 import java.io.Serializable;
 
-public record PartyMember(String name, int health, boolean isAlive, int foodConsumed, boolean hasDisease) implements Serializable {
+public record PartyMember(String name, int health, boolean isAlive, int foodConsumed, boolean hasDisease, boolean hasBeenBuried) implements Serializable {
     public PartyMember newHealth(int health) {
-        return new PartyMember(name, health, isAlive, foodConsumed, hasDisease);
+        return new PartyMember(name, health, isAlive, foodConsumed, hasDisease, hasBeenBuried);
     }
 
     public PartyMember newAlive(boolean isAlive) {
-        return new PartyMember(name, health, isAlive, foodConsumed, hasDisease);
+        return new PartyMember(name, health, isAlive, foodConsumed, hasDisease, hasBeenBuried);
     }
 
     public PartyMember newFoodConsumed(int foodConsumed) {
-        return new PartyMember(name, health, isAlive, foodConsumed, hasDisease);
+        return new PartyMember(name, health, isAlive, foodConsumed, hasDisease, hasBeenBuried);
     }
 
     public PartyMember newDisease(boolean hasDisease) {
-        return new PartyMember(name, health, isAlive, foodConsumed, hasDisease);
+        return new PartyMember(name, health, isAlive, foodConsumed, hasDisease, hasBeenBuried);
+    }
+
+    public PartyMember newBuried(boolean hasBeenBuried) {
+        return new PartyMember(name, health, isAlive, foodConsumed, hasDisease, hasBeenBuried);
     }
 }
