2e684415263d437ad21c815c1399ad80c5a375a3
hlpdev <hunterpollock2006@gmail.com>
2025-02-13 19:46:17 -0500

    added trading, speaking to members of your group

--- src/main/java/hlpdev/oregontrail/game/Progress.java
+++ src/main/java/hlpdev/oregontrail/game/Progress.java
@@ -1,19 +1,25 @@
 package hlpdev.oregontrail.game;
 
+import com.googlecode.lanterna.SGR;
 import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.gui2.*;
 import com.googlecode.lanterna.gui2.dialogs.MessageDialog;
+import com.googlecode.lanterna.gui2.dialogs.MessageDialogButton;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.terminal.Terminal;
 import hlpdev.oregontrail.*;
 import hlpdev.oregontrail.enums.Location;
 import hlpdev.oregontrail.enums.Pace;
 import hlpdev.oregontrail.enums.Weather;
+import hlpdev.oregontrail.npcs.Trade;
+import hlpdev.oregontrail.npcs.Trading;
 import hlpdev.oregontrail.records.PartyMember;
 import hlpdev.oregontrail.util.Math;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Progress {
     public static void Execute(Terminal terminal, Screen screen) {
@@ -50,48 +56,74 @@ public class Progress {
             rationsVisual = "Members are starving";
         }
 
+        String staminaVisual;
+        if (Main.GameState.stamina > 70) {
+            staminaVisual = "High";
+        } else if (Main.GameState.stamina > 30) {
+            staminaVisual = "Normal";
+        } else {
+            staminaVisual = "Low";
+        }
+
+        String moraleVisual;
+        if (Main.GameState.morale > 70) {
+            moraleVisual = "Good";
+        } else if (Main.GameState.morale > 40) {
+            moraleVisual = "Ok";
+        } else {
+            moraleVisual = "Bad";
+        }
+
         final WindowBasedTextGUI textGui = new MultiWindowTextGUI(screen);
         final Window window = new BasicWindow();
         window.setFixedSize(new TerminalSize(62, 28));
         window.setHints(List.of(Window.Hint.CENTERED));
+
+        if (atPointOfInterest && pointOfInterest != Location.INDEPENDENCE && pointOfInterest != Location.OREGON_CITY) {
+            MessageDialog.showMessageDialog(textGui, "Attention", "You have reached a point of interest! You may buy supplies and attempt to trade!", MessageDialogButton.Continue);
+        }
 
         Panel panel = new Panel(new AbsoluteLayout());
 
         Label titleLabel = getTitleLabel(atPointOfInterest, pointOfInterest, closestLocation);
         titleLabel.setPosition(new TerminalPosition(2, 1));
         titleLabel.setSize(new TerminalSize(30, 2));
+        titleLabel.addStyle(SGR.BOLD);
         panel.addComponent(titleLabel);
 
         Label statusLabel = new Label(String.format("""
                   Weather: %s
                   Health: %s
+                  Stamina: %s
                   Pace: %s
+                  Morale: %s
                   Rations: %s
-                """, currentWeather.visualRepresentation, healthVisual, Main.GameState.pace.visualRepresentation, rationsVisual));
+                """, currentWeather.visualRepresentation, healthVisual, staminaVisual, Main.GameState.pace.visualRepresentation, moraleVisual, rationsVisual));
         statusLabel.setPosition(new TerminalPosition(2, 4));
-        statusLabel.setSize(new TerminalSize(40, 4));
+        statusLabel.setSize(new TerminalSize(40, 6));
         panel.addComponent(statusLabel);
 
         Label youMay = new Label("You may:");
-        youMay.setPosition(new TerminalPosition(2, 10));
+        youMay.setPosition(new TerminalPosition(2, 12));
         youMay.setSize(new TerminalSize(8, 1));
         panel.addComponent(youMay);
 
         Button continueButton = new Button("Continue on the trail");
-        continueButton.setPosition(new TerminalPosition(2, 12));
+        continueButton.setPosition(new TerminalPosition(2, 14));
         continueButton.setSize(new TerminalSize(38, 1));
         panel.addComponent(continueButton);
 
         Button suppliesButton = new Button("Check Supplies");
-        suppliesButton.setPosition(new TerminalPosition(2, 13));
+        suppliesButton.setPosition(new TerminalPosition(2, 15));
         suppliesButton.setSize(new TerminalSize(38, 1));
         panel.addComponent(suppliesButton);
         suppliesButton.addListener((_) -> {
+            window.close();
             CheckSupplies(terminal, screen);
         });
 
         Button mapButton = new Button("Look at the map");
-        mapButton.setPosition(new TerminalPosition(2, 14));
+        mapButton.setPosition(new TerminalPosition(2, 16));
         mapButton.setSize(new TerminalSize(38, 1));
         panel.addComponent(mapButton);
         mapButton.addListener((_) -> {
@@ -100,7 +132,7 @@ public class Progress {
         });
 
         Button paceButton = new Button("Change pace");
-        paceButton.setPosition(new TerminalPosition(2, 15));
+        paceButton.setPosition(new TerminalPosition(2, 17));
         paceButton.setSize(new TerminalSize(38, 1));
         panel.addComponent(paceButton);
         paceButton.addListener((_) -> {
@@ -109,27 +141,40 @@ public class Progress {
         });
 
         Button restButton = new Button("Stop to rest");
-        restButton.setPosition(new TerminalPosition(2, 16));
+        restButton.setPosition(new TerminalPosition(2, 18));
         restButton.setSize(new TerminalSize(38, 1));
         panel.addComponent(restButton);
 
         Button tradeButton = new Button("Attempt to trade");
-        tradeButton.setPosition(new TerminalPosition(2, 17));
+        tradeButton.setPosition(new TerminalPosition(2, 19));
         tradeButton.setSize(new TerminalSize(38, 1));
         panel.addComponent(tradeButton);
+        tradeButton.addListener((_) -> {
+            if (!atPointOfInterest) {
+                MessageDialog.showMessageDialog(textGui, "Whoops", "You're currently in the middle of no where, so you can't find anyone to trade with.", MessageDialogButton.OK);
+                return;
+            }
+
+            window.close();
+            AttemptToTrade(terminal, screen);
+        });
 
         Button talkButton = new Button("Talk to people");
-        talkButton.setPosition(new TerminalPosition(2, 18));
+        talkButton.setPosition(new TerminalPosition(2, 20));
         talkButton.setSize(new TerminalSize(38, 1));
         panel.addComponent(talkButton);
+        talkButton.addListener((_) -> {
+            window.close();
+            TalkToPeople(terminal, screen);
+        });
 
         Button buySuppliesButton = new Button("Buy supplies");
-        buySuppliesButton.setPosition(new TerminalPosition(2, 19));
+        buySuppliesButton.setPosition(new TerminalPosition(2, 21));
         buySuppliesButton.setSize(new TerminalSize(38, 1));
         panel.addComponent(buySuppliesButton);
         buySuppliesButton.addListener((_) -> {
             if (!atPointOfInterest) {
-                MessageDialog.showMessageDialog(textGui, "Whoops", "You're currently in the middle of no where, so you can't find a shop to purchase supplies at.");
+                MessageDialog.showMessageDialog(textGui, "Whoops", "You're currently in the middle of no where, so you can't find a shop to purchase supplies at.", MessageDialogButton.OK);
                 return;
             }
 
@@ -306,6 +351,142 @@ public class Progress {
         panel.addComponent(goBackButton);
         goBackButton.addListener((_) -> {
             Main.GameState.pace = Pace.fromString(paceInput.getSelectedItem());
+            window.close();
+            Execute(terminal, screen);
+        });
+
+        window.setComponent(panel);
+        textGui.addWindowAndWait(window);
+    }
+
+    private static void AttemptToTrade(Terminal terminal, Screen screen) {
+        @NotNull Location pointOfInterest = Objects.requireNonNull(Location.getPointOfInterest(Main.GameState.totalDistanceTraveled));
+
+        final WindowBasedTextGUI textGui = new MultiWindowTextGUI(screen);
+        final Window window = new BasicWindow();
+        window.setFixedSize(new TerminalSize(62, 28));
+        window.setHints(List.of(Window.Hint.CENTERED));
+
+        Panel panel = new Panel(new AbsoluteLayout());
+
+        if (pointOfInterest.tradesAvailable <= 0) {
+            MessageDialog.showMessageDialog(textGui, "", "You can't find anyone to trade with.", MessageDialogButton.OK);
+            window.close();
+            Execute(terminal, screen);
+        }
+
+        Trade trade = Trading.getRandomTrade();
+        if (Objects.requireNonNull(trade).Execute(textGui, window, panel)) {
+            pointOfInterest.tradesAvailable -= 1;
+        }
+
+        Execute(terminal, screen);
+    }
+
+    private static void TalkToPeople(Terminal terminal, Screen screen) {
+        final WindowBasedTextGUI textGui = new MultiWindowTextGUI(screen);
+        final Window window = new BasicWindow();
+        window.setFixedSize(new TerminalSize(62, 28));
+        window.setHints(List.of(Window.Hint.CENTERED));
+
+        Panel panel = new Panel(new AbsoluteLayout());
+
+        Label label = new Label("""
+                Which person in your group
+                would you like to speak to?""");
+        label.setPosition(new TerminalPosition(2, 2));
+        label.setSize(new TerminalSize(40, 2));
+        panel.addComponent(label);
+
+        RadioBoxList<String> people = new RadioBoxList<>();
+        people.setPosition(new TerminalPosition(2, 5));
+        for (PartyMember member : Main.GameState.partyMembers) {
+            if (member.isAlive()) {
+                people.addItem(member.name());
+            }
+        }
+        people.setSize(new TerminalSize(40, people.getItemCount()));
+        panel.addComponent(people);
+
+        Button continueButton = new Button("Continue");
+        continueButton.setPosition(new TerminalPosition(2, 7 + people.getItemCount()));
+        continueButton.setSize(new TerminalSize(12, 1));
+        panel.addComponent(continueButton);
+        continueButton.addListener((_) -> {
+            if (people.getCheckedItemIndex() == -1) {
+                MessageDialog.showMessageDialog(textGui, "Error", "You need to choose a party member to speak to!", MessageDialogButton.Retry);
+                return;
+            }
+
+            window.close();
+            TalkToPerson(terminal, screen, people.getCheckedItem());
+        });
+
+        Button goBack = new Button("Go back");
+        goBack.setPosition(new TerminalPosition(2, 8 + people.getItemCount()));
+        goBack.setSize(new TerminalSize(11, 1));
+        panel.addComponent(goBack);
+        goBack.addListener((_) -> {
+            window.close();
+            Execute(terminal, screen);
+        });
+
+        window.setComponent(panel);
+        textGui.addWindowAndWait(window);
+    }
+
+    private static void TalkToPerson(Terminal terminal, Screen screen, String name) {
+        @NotNull PartyMember member = Main.GameState.partyMembers.stream().filter(partyMember -> partyMember.name().equals(name)).findFirst().get();
+
+        final WindowBasedTextGUI textGui = new MultiWindowTextGUI(screen);
+        final Window window = new BasicWindow();
+        window.setFixedSize(new TerminalSize(62, 28));
+        window.setHints(List.of(Window.Hint.CENTERED));
+
+        Panel panel = new Panel(new AbsoluteLayout());
+
+        String healthVisual;
+        if (member.health() > 80) {
+            healthVisual = "good.";
+        } else if (member.health() > 60) {
+            healthVisual = "not ok, and they have\nminor injuries.";
+        } else if (member.health() > 30) {
+            healthVisual = "not ok, and they have\nmajor injuries.";
+        } else {
+            healthVisual = "critical and they\nare dying.";
+        }
+
+        Label label = new Label(String.format("""
+                You speak to %s and they say that
+                their health is %s
+                """, member.name(), healthVisual));
+        label.setPosition(new TerminalPosition(2, 2));
+        label.setSize(new TerminalSize(45, 3));
+        panel.addComponent(label);
+
+        Button giveMedicine = new Button("Give Medicine");
+        giveMedicine.setPosition(new TerminalPosition(2, 7));
+        giveMedicine.setSize(new TerminalSize(17, 1));
+        panel.addComponent(giveMedicine);
+        giveMedicine.addListener((_) -> {
+            if (Main.GameState.medicine == 0) {
+                MessageDialog.showMessageDialog(textGui, "", "You have no medicine left in your wagon!", MessageDialogButton.OK);
+                return;
+            }
+
+            Main.GameState.partyMembers.remove(member);
+            Main.GameState.partyMembers.add(member.newHealth(java.lang.Math.min(member.health() + 45, 100)));
+
+            MessageDialog.showMessageDialog(textGui, "", String.format("You gave %s medicine...", member.name()), MessageDialogButton.OK);
+            window.close();
+            TalkToPerson(terminal, screen, member.name());
+        });
+
+        Button goBack = new Button("Go back");
+        goBack.setPosition(new TerminalPosition(2, 8));
+        goBack.setSize(new TerminalSize(11, 1));
+        panel.addComponent(goBack);
+        goBack.addListener((_) -> {
             window.close();
             Execute(terminal, screen);
         });
